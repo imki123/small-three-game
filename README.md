5b60ae6ec836b83695a3ebc62ca1753de931ee8c
Hoodie <hoodie@haechi.io>
2023-12-17 20:03:43 +0900

    chore: init project

---
+++ README.md
@@ -0,0 +1,11 @@
+# small-three-game
+
+A small game with three.js
+
+## How to run
+
+```bash
+  npm i -g pnpm
+  pnpm i
+  pnpm dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <>
+      <div>simple three game</div>
+      <div className="screen">screen</div>
+    </>
+  )
+}
